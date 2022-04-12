9ef7b87e272b73a2f30d2094abfb37a368f55571
Fariz Fahmi <farizfahmi674@gmail.com>
2022-04-13 03:13:12 +0700

    add config database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import 'dotenv/config'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    logging: true,
+    entities: [],
+    autoLoadEntities: true,
+    synchronize: true,
+  })]
 })
 export class AppModule {}
